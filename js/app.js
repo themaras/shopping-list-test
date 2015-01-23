a14d824290c9744e57ae9e76080b5690249573ac
Themistoklis Papaioannou <themaras3@gmail.com>
2015-01-23 15:02:37 +0200

    final commit

--- js/app.js
+++ js/app.js
@@ -2,35 +2,35 @@ $(document).ready(function () {
     $('.add-button').click(function () {
         var input = $('.add-items').val();
         if ($('.add-items').val().length === 0) {
-            alert('put something here');
+            alert('Put Something Here!');
             return false;
         }
         $('.ul').append('<li><button class="delete">Delete</button>' + input + '<button class="gotit">gotit!</button></li>');
     });
 
 
-    $('.add-items').keypress(function (e) {
+$('.add-items').keypress(function (e) {
         if (e.which == 13) {
             if ($('.add-items').val().length === 0) {
-                alert('put something here');
-                return false;
+            alert('Put Something Here!');
+            return false;
             }
             var input = $('.add-items').val();
             $('.ul').append('<li><button class="delete">Delete</button>' + input + '<button class="gotit">gotit!</button></li>');
         }
     });
 
-    $('.deleteall').click(function () {
-        $("ul").empty();
+$('.deleteall').click(function () {
+    $("ul").empty();
     });
     
  
-    $('ul').on('click','.delete:button', function() {
-$(this).closest('li').remove();
+$('ul').on('click','.delete:button', function() {
+    $(this).closest('li').remove();
     });
     
-    $('ul').on('click','.gotit:button', function() {
-$(this).closest('li').toggleClass("stroked");
+$('ul').on('click','.gotit:button', function() {
+    $(this).closest('li').toggleClass("stroked");
     });
  
  });
